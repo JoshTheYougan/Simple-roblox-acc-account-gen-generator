58cd5c3b47fcde85d521df1202d63ff3d8025ce5
Josh <105233121+JoshTheYougan@users.noreply.github.com>
2024-02-22 08:31:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-#sorry this is not a roblox gen anymore its a cheatsheat
-telnet- is most of the time at port 23/tcp and we can connect to it by telnet (ip)
-ftp - is an file transfer protocol and is on port 21 most of the time, we can use ftp -h for help
-ping - we can use it to find if there is connection between us and the targer
-ls - is the command that we can display file with
-get - is an command used on ftp server to download files
+nothing
